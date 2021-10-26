e531b0347e0894d7511c94d5fd3f89dd9490348c
Nico Keskin <nicokeskin@Nicos-Air.fritz.box>
2021-10-26 08:33:17 +0200

    initial commit

---
+++ backend/src/test/java/nicokeskin/applicationmanager/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nicokeskin.applicationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
