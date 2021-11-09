3f991a9b4f9429ca03ee1304b23920fbfb3410c1
nikeskin <73691854+nikeskin@users.noreply.github.com>
2021-11-09 12:07:19 +0100

    Update DocumentationArea.js

--- frontend/src/components/DocumentationArea.js
+++ frontend/src/components/DocumentationArea.js
@@ -6,8 +6,6 @@ import IconButton from '@mui/material/IconButton';
 
 export default function DocumentationArea({ providedDocumentation, missingDocumentation }) {
 
-    //Todo use one component and have just one list
-
     const getDocumentationFieldName = (typeNumber) => {
         const typeNumberToOutputMatrix = {
             1: "Concept of Roles and Rights",
@@ -33,8 +31,6 @@ export default function DocumentationArea({ providedDocumentation, missingDocume
     }
 
     return (
-
-        // Todo hardcode list / no map needed for provided and missing
         <Layout>
             <Provided>
                 {providedDocumentation.length > 0 && <ProvidedHeadline>Provided:</ProvidedHeadline>}
