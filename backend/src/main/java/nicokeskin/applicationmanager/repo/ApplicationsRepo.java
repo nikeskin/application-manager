b6ad4e4c89d66ab5402080b67ac99e0bd8e1d6b9
nikeskin <nico_keskin_91@hotmail.de>
2021-11-12 11:48:02 +0100

    Add changes from issue3 PR

--- backend/src/main/java/nicokeskin/applicationmanager/repo/ApplicationsRepo.java
+++ backend/src/main/java/nicokeskin/applicationmanager/repo/ApplicationsRepo.java
@@ -1,15 +1,12 @@
 package nicokeskin.applicationmanager.repo;
 
 import nicokeskin.applicationmanager.model.Application;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ApplicationsRepo extends PagingAndSortingRepository<Application, String> {
-
-    List<Application> findAll();
+public interface ApplicationsRepo extends MongoRepository<Application, String> {
 
     Application findFirstByOrderByAppIdDesc();
+
 }
