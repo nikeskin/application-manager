add555ee9eb7e69b9f28979f56ca3616510c6926
nikeskin <nico_keskin_91@hotmail.de>
2021-11-12 13:15:00 +0100

    remove length in useDocu hook

--- frontend/src/hooks/useDocumentation.js
+++ frontend/src/hooks/useDocumentation.js
@@ -5,40 +5,41 @@ export default function useDocumentation(application) {
     const providedDocumentation = [];
     const missingDocumentation = [];
 
-    for (const documentationKey in documentation) {
+        for (const documentationKey in documentation) {
 
-        const checkedDocumentation = {}
-        const key = documentationKey;
-        const type = "type";
+            const checkedDocumentation = {}
+            const key = documentationKey;
+            const type = "type";
 
-        switch (key) {
-            case "conceptOfRolesAndRights":
-                checkedDocumentation[type] = 1;
-                break;
-            case "supplierContract":
-                checkedDocumentation[type] = 2;
-                break;
-            case "technicalDesignConcept":
-                checkedDocumentation[type] = 3;
-                break;
-            case "testingConcept":
-                checkedDocumentation[type] = 4;
-                break;
-            case "userHandbook":
-                checkedDocumentation[type] = 5;
-                break;
-            default: checkedDocumentation[type] = 6;
+            switch (key) {
+                case "conceptOfRolesAndRights":
+                    checkedDocumentation[type] = 1;
+                    break;
+                case "supplierContract":
+                    checkedDocumentation[type] = 2;
+                    break;
+                case "technicalDesignConcept":
+                    checkedDocumentation[type] = 3;
+                    break;
+                case "testingConcept":
+                    checkedDocumentation[type] = 4;
+                    break;
+                case "userHandbook":
+                    checkedDocumentation[type] = 5;
+                    break;
+                default: checkedDocumentation[type] = 6;
+            }
+
+            if (!documentation[documentationKey]) {
+                console.log(documentation[documentationKey])
+                checkedDocumentation[key] = documentation[documentationKey];
+                missingDocumentation.push(checkedDocumentation);
+            } else {
+                checkedDocumentation[key] = documentation[documentationKey];
+                providedDocumentation.push(checkedDocumentation);
+            }
+
         }
-
-        if (documentation[documentationKey].length === 0) {
-            checkedDocumentation[key] = documentation[documentationKey];
-            missingDocumentation.push(checkedDocumentation);
-        } else {
-            checkedDocumentation[key] = documentation[documentationKey];
-            providedDocumentation.push(checkedDocumentation);
-        }
-
-    }
 
     const documentationStatus = providedDocumentation.length > 0 ? (providedDocumentation.length / (providedDocumentation.length + missingDocumentation.length)*100) : 0;
 
